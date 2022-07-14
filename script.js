4b6ae1c4f80b0a80e7aa2f3922137bf653e26716
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-07-13 22:12:07 -0300

    Select the weather type

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const select = document.querySelector('select');
-const para = document.querySelector('p');
+const para = document.getElementById('p1');
+const forChoice = document.querySelector("p");
 
 select.addEventListener('change', setWeather);
 
@@ -7,14 +8,26 @@ function setWeather() {
   const choice = select.value;
 
   if (choice === 'sunny') {
-    para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.';
+    para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach,\
+        or the park, and get an ice cream.';
+    forChoice.textContent="☀";
   } else if (choice === 'rainy') {
-    para.textContent = 'Rain is falling outside; take a rain coat and an umbrella, and don\'t stay out for too long.';
+    para.textContent = 'Rain is falling outside; take a rain coat and an umbrella, \
+    and don\'t stay out for too long.';
+    forChoice.textContent = "🌧";
   } else if (choice === 'snowing') {
-    para.textContent = 'The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
+    para.textContent = 'The snow is coming down — it is freezing! Best to stay in \
+    with a cup of hot chocolate, or go build a snowman.';
+    forChoice.textContent = "⛄";
   } else if (choice === 'overcast') {
-    para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
+    para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could \
+    turn any minute, so take a rain coat just in case.';
+    forChoice.textContent = "🌡";
   } else {
     para.textContent = '';
   }
 }
+
+textContent
+
+choice === select.value;
