7b4d366b0f69802f9f7c345addca25d99d4fec02
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-07-15 19:20:46 -0300

    conditional mad with if else ? : switch case break

--- script.js
+++ script.js
@@ -8,10 +8,24 @@
     
 // }
 
-let realName = prompt("What is the \"official\" name of JavaScript?");
+// let realName = prompt("What is the \"official\" name of JavaScript?");
 
-let message =
-(realName == "ECMAScript")? "Right!":
-"You don't know ECMAScript?!";
+// let message =
+// (realName == "ECMAScript")? "Right!":
+// "You don't know ECMAScript?!";
 
-document.getElementById("p1").innerHTML = message;
+// document.getElementById("p1").innerHTML = message;
+
+let realName = prompt("What is the \"official\" name of JavaScript?", "ECMAScript");
+
+switch(realName){
+    case  "ECMAScript":
+      document.getElementById("p1").innerHTML = "Right!";
+    break;
+    case "me":
+      document.getElementById("p1").innerHTML = "You gotta be kitting me!";
+    break;
+ default:
+  document.getElementById("p1").innerHTML = "You don't know ECMAScript? What a shame!";
+}
+
