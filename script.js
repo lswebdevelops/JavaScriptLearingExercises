d5b5acfbd97638391c1cdaa5f5265c3dd46852c2
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-07-26 22:00:45 -0300

    using continue: if !==squareRoot, continue

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-const contacts = ['Luciano:99655324', 'Adiles:96532324', 'Daiana:93922423', 'Renato:9998769', 'Davi:9384975'];
-
-const param = document.querySelector('p');
+const para = document.querySelector('p');
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
 
-btn.addEventListener('click', () =>{
-
-  const searchName = input.value.toLowerCase();
-
-  input.value = "";
+btn.addEventListener('click', () => {
+  para.textContent = 'Output: ';
+  const num = input.value;
+  input.value = '';
   input.focus();
-
-  param.textContent= "";
-  for(const contact of contacts){
-    const splitContact = contact.split(":");
-    if(splitContact[0].toLowerCase() === searchName){
-      param.textContent = splitContact[0] + '\'s number is ' + splitContact[1] +
-      ".";
-      break;
+  for (let i = 1; i <= num; i++) {
+    let sqRoot = Math.sqrt(i);
+    if (Math.floor(sqRoot) !== sqRoot) {
+      continue;
     }
-
+    para.textContent += `${i} `;
   }
-if(param.textContent === ''){
-  param.textContent = 'Contact not found.';
-}
-
-})
-
+});
