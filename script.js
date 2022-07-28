37b4dab2da51c376bfe01929935f41fc812ddbbb
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-07-27 22:06:53 -0300

    Adding a period at the end of a printet array of cats

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
+const cats = ['Pete', 'Biggles', 'Jasmine'];
+ 
 
-const selectors = document.querySelectorAll('p.example, div.example2, span.example');
-
-for(i = 0; i <selectors.length; i ++){
-    selectors[i].style.backgroundColor = 'yellow';
-    selectors[i].style.color = 'firebrick';
-    selectors[i].style.borderRadius = "25px 32px 23px 43px";
-  
+let myCatsAreCalled = "My cats are called: ";
 
 
+for (let i = 0; i <cats.length; i++){
+    
+
+    if(i === cats.length -1){
+        myCatsAreCalled+= `and the last one is ${cats[i]}.`
+    }else{
+        myCatsAreCalled += `${cats[i]}, `;
+    }
+    
 }
 
-const hOnes = document.querySelectorAll('h1, h2');
-console.log(hOnes.length)
-for(j=0; j < hOnes.length; j++){
-    hOnes[j].style.color = 'red';
-
-    hOnes[j].style.textShadow = "5px 5px 1px #333, 5px 5px 1px grey";
-
-}
-
-const pIs = document.querySelectorAll('p');
-console.log(pIs.length)
-for(k=0; k < pIs.length; k++){
-    pIs[k].style.color = 'lightgreen';
-
-    pIs[k].style.textShadow = "5px 5px 1px blue, 5px 5px 1px green";
-
-}
+    document.querySelector('p').innerHTML = myCatsAreCalled;
