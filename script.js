28b9886565738900711b90259884e445adde2439
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-07-27 21:30:05 -0300

    adding style using query selectors

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-const cats = ['Pete', 'Biggles', 'Jasmine'];
 
-let myFavoriteCats = 'My cats are called ';
+const selectors = document.querySelectorAll('p.example, div.example2, span.example');
 
-let i = 0;
+for(i = 0; i <selectors.length; i ++){
+    selectors[i].style.backgroundColor = 'yellow';
+    selectors[i].style.color = 'firebrick';
+    selectors[i].style.borderRadius = "25px 32px 23px 43px";
+  
 
-while (i < cats.length) {
-  if (i === cats.length - 1) {
-    myFavoriteCats += `and ${cats[i]}.`;
-  } else {
-    myFavoriteCats += `${cats[i]}, `;
-  }
 
-  i++;
 }
 
+const hOnes = document.querySelectorAll('h1, h2');
+console.log(hOnes.length)
+for(j=0; j < hOnes.length; j++){
+    hOnes[j].style.color = 'red';
 
-document.getElementById('p').innerHTML = myFavoriteCats;
-// "My cats are called Pete, Biggles, and Jasmine."
+    hOnes[j].style.textShadow = "5px 5px 1px #333, 5px 5px 1px grey";
+
+}
+
+const pIs = document.querySelectorAll('p');
+console.log(pIs.length)
+for(k=0; k < pIs.length; k++){
+    pIs[k].style.color = 'lightgreen';
+
+    pIs[k].style.textShadow = "5px 5px 1px blue, 5px 5px 1px green";
+
+}
