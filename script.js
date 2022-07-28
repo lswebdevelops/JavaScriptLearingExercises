7640958a7d1a7c425ca9e49bd526777c0bb57fdd
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-07-28 09:34:28 -0300

    loop using continue

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
-const contacts = [
-    'Luciano:2232322:Indaial', 
-    'Davi:3453456:Indaial', 
-    'Adiles:7654322:Indaial', 
-    'Beto:9998769:Salete', 
-    'Daiana:9384975:Blumenau', 
-    'Renato:32278020:Salete'
-    ];
+const para = document.querySelector('p');
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
 
-    const para = document.querySelector('p');
-    const input = document.querySelector('input');
-    const btn = document.querySelector('button');
-
-        btn.addEventListener('click', () => {
-
-        const searchName = input.value.toLowerCase();
-    // input.value= "";
-    //after searching for a name the cursor returns to the input field with 'focus'
-    input.focus();
-    para.textContent = '';
-
-    for(const contact of contacts){
-        const splitContact = contact.split(':');
-        
-        if(splitContact[0].toLowerCase()=== searchName){
-            para.textContent =
-             splitContact[0] + '\'s number is ' 
-            + splitContact[1]+ " and he/she lives in "
-             + splitContact[2] + ".";
-            break;
-        }
-
+btn.addEventListener('click', () => {
+  para.textContent = 'Output: ';
+  const num = input.value;
+  input.value = '';
+  input.focus();
+  for (let i = 1; i <= num; i++) {
+    let sqRoot = Math.sqrt(i);
+    if (Math.floor(sqRoot) !== sqRoot) {
+      continue;
     }
-    if(para.textContent === ""){
-        para.textContent = "Contact not found. Please be a doll and take it seriously!";
-    }
+    para.textContent += `${i} `;
+  }
 });
-
-
-
