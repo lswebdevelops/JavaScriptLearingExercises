68f1a9ca65f2ea7d015e38096d83c589e9ac139d
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-07-27 22:44:18 -0300

    searching for a name in a list using for loop with break

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-const cats = ['Pete', 'Biggles', 'Jasmine'];
- 
+const contacts = [
+    'Luciano:2232322:Indaial', 
+    'Davi:3453456:Indaial', 
+    'Adiles:7654322:Indaial', 
+    'Beto:9998769:Salete', 
+    'Daiana:9384975:Blumenau', 
+    'Renato:32278020:Salete'
+    ];
 
-let myCatsAreCalled = "My cats are called: ";
+    const para = document.querySelector('p');
+    const input = document.querySelector('input');
+    const btn = document.querySelector('button');
 
+        btn.addEventListener('click', () => {
 
-for (let i = 0; i <cats.length; i++){
-    
+        const searchName = input.value.toLowerCase();
+    // input.value= "";
+    //after searching for a name the cursor returns to the input field with 'focus'
+    input.focus();
+    para.textContent = '';
 
-    if(i === cats.length -1){
-        myCatsAreCalled+= `and the last one is ${cats[i]}.`
-    }else{
-        myCatsAreCalled += `${cats[i]}, `;
+    for(const contact of contacts){
+        const splitContact = contact.split(':');
+        
+        if(splitContact[0].toLowerCase()=== searchName){
+            para.textContent =
+             splitContact[0] + '\'s number is ' 
+            + splitContact[1]+ " and he/she lives in "
+             + splitContact[2] + ".";
+            break;
+        }
+
     }
-    
-}
+    if(para.textContent === ""){
+        para.textContent = "Contact not found. Please be a doll and take it seriously!";
+    }
+});
 
-    document.querySelector('p').innerHTML = myCatsAreCalled;
+
+
