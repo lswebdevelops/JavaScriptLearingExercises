f9b49c4e0e2ed52cde5ade4d5fb1b8d7c3885f89
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-05 19:43:35 -0300

    black jack counting to bet/hold using switch/case

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-// Setup
-function abTest(a, b) {
+let count = 0;
+
+function cc(card) {
   // Only change code below this line
 
-if(a < 0 || b < 0 ){
+ let result = "";
 
-   return undefined;
+switch(card){
+ 
+case 2:
+ case 3:
+ case 4:
+case 5:
+case 6:
+count ++;
+break;
+
+case 10:
+case 'J':
+case 'Q':
+case 'K':
+case 'A':
+count --;
+break;
 }
+
+if(count > 0){
+result = `${count} Bet`;
+}else{
+  result = `${count} Hold`;
+}
+  return result;
   // Only change code above this line
-
-  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
 
-document.querySelector('#p1').innerHTML = abTest(2,-2);
+//  cc(3); cc(7); cc('K'); cc('A');
+
+document.querySelector('#p1').innerHTML = cc('Q');
