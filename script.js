eeadaea10b214a15c5fd2d5750dfa3b3c9ef6635
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-05 21:53:18 -0300

    Testing object's properties

--- script.js
+++ script.js
@@ -1,45 +1,20 @@
-// Setup
-function phoneticLookup(val) {
-  let result = "";
-  // Only change code below this line
+function testObj(obj, checkProp){
 
 
- let lookup= {
-     "alpha": "Adams",
-     "bravo": "Boston",
-      "charlie": "Chicago",
-     "delta": "Denver",
-      "echo": "Easy",
-      "foxtrot": "Frank"
-  };
-  // switch(val) {
-  //   case "alpha":
-  //     result = "Adams";
-  //     break;
-  //   case "bravo":
-  //     result = "Boston";
-  //     break;
-  //   case "charlie":
-  //     result = "Chicago";
-  //     break;
-  //   case "delta":
-  //     result = "Denver";
-  //     break;
-  //   case "echo":
-  //     result = "Easy";
-  //     break;
-  //   case "foxtrot":
-  //     result = "Frank";
-  // }
-result = lookup[val];
-  // Only change code above this line
-  return result;
+  if(obj.hasOwnProperty(checkProp)){
+    return obj[checkProp];
+  }
+return "Not found";
 }
 
 
-document.querySelector('#p2').innerHTML = phoneticLookup("alpha") 
 
-//  document.querySelector('#p3').innerHTML = lastLetter;
+const myObj = {
+  top: "hat",
+  bottom: "pants"
+};
+
+ document.querySelector('#p3').innerHTML = testObj(myObj , 'bottom');
 
 
 
