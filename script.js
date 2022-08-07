6519655c6790e7239a00a9c8a760d3258dd68823
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-06 21:31:05 -0300

    using a function to change object values

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-const myPlants = [
-  {
-    type: "flowers",
-    list: [
-      "rose",
-      "tulip",
-      "dandelion"
-    ]
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
   },
-  {
-    type: "trees",
-    list: [
-      "fir",
-      "pine",
-      "birch"
-    ]
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
   }
-];
+};
 
-const secondTree = myPlants[1].list[1];
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+// If prop isn't tracks and value isn't an empty string, update or set that album's prop to value.
+if(prop !==  'tracks' && value !== ""){
+   records[id][prop]= value;
+}
+// If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it.
+else if(prop ===  'tracks' &&  records[id].hasOwnProperty('tracks') === false){
+  records[id][prop] = [value];
+}
+// If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
+else if(prop ===  'tracks' && value !== ""){
+  records[id][prop].push(value);
+}
+// If value is an empty string, delete the given prop property from the album.
+else if(value === ""){
+  delete  records[id][prop];
+}
 
 
- 
- document.querySelector('#p1').innerHTML = secondTree;
-// document.querySelector('#p2').innerHTML = ourPets[1].names[0];
-
-
-
-
-
-
+  return records;
+}
+// Note: A copy of the recordCollection object is used for the tests.
+console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
 
