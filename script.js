77844faafe2fe12c55aa0b6c2fdafb007fba2891
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-06 23:11:30 -0300

    creating loops using a while loop

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-const someAdjective = "smart";
-let myStr = "Learning to code is ";
-myStr += someAdjective;
+const ourArray =[]
+let i = 0;
+while(i < 5){
+  ourArray.push(`${i} dog(s) bought`);
+  i++;
+}
 
-document.querySelector("#p1").innerHTML = someAdjective;
-document.querySelector("#p2").innerHTML = myStr;
-
-// Change code below this line
-console.log(myStr.length)
-console.log("Learning to code is ".length)
-
-let lastLetterOfAdj = someAdjective[someAdjective.length -1]
-console.log(lastLetterOfAdj)
-
-let secondlastLetterOfAdj = "";
-console.log( secondlastLetterOfAdj = someAdjective[someAdjective.length -2])
+console.log(ourArray)
