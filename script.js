cd1398a73333a09f1d1018e9e88eb19f02477faa
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-08 20:54:46 -0300

    looping through an array using for, return

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
-// this was a multiply function used for a sum function//
-// function multiply(arr, n) {
-//   let product = 0;
-//   for (let i = 0; i < n; i++) {
-//     product += arr[i];
-//   }
-//   return product;
-// }
-// let number = prompt("Type a number", 3);
-// let array = [];
-// for(let i = 1; i <= number; i ++){
-//   array.push(i)
-// }
-// console.log(`Items of \narray: ${array}`);
-// let result = multiply(array, array.length);
-// console.log(`Sum of \narray: ${result}`)
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
 
+function lookUpProfile(name, prop) {
 
-// now solving it as an recursive function: 
-function sum(arr, n) {
-  // Only change code below this line
- if (n <= 0) {
-      return 0;
-    } else {
-
-      return sum(arr, n - 1) + arr[n - 1];
+  for (let i = 0; i < contacts.length; i++){
+      if(contacts[i].firstName === name){
+        if(contacts[i].hasOwnProperty(prop)){
+          return contacts[i][prop];
+        }else{
+          return 'No such property';
+        }
+      }
     }
-  // Only change code above this line
+  return "No such contact";
 }
-let number = prompt("Type a number", 3);
-let array = [];
-for(let i = 1; i <= number; i ++){
-  array.push(i)
-}
-console.log(`Items of \narray: ${array}`);
-let result = sum(array, array.length);
-console.log(`Sum of \narray: ${result}`)
+
+console.log(lookUpProfile("Sherlock", "lastName"))
