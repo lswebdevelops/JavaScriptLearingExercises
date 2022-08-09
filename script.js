b7b0dbbdd095e65bfa395ec913ad8474d019628f
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-08 21:56:02 -0300

    ternary function using condicional: return ? :

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-let binaryNumber = prompt("Type binary number:", "1010001001");
-function convertToInteger(str) {
 
-  let convertedInt = parseInt(binaryNumber, 2);
-  return convertedInt;
+
+let a = prompt("give me a number", 3);
+let b = prompt("give me a number", 5);
+
+function findGreater(a, b) {
+  if(a > b) {
+    return "a is greater";
+  }
+  else {
+    return "b is greater or equal";
+  }
 }
 
-let number = convertToInteger("1001");
- console.log(`Binary Number: ${binaryNumber}`);
-console.log(`Decimal Number: ${number}`);
+
+
+
+function findGreaters(a, b) {
+  return a > b ? "a is greater" : "b is greater or equal";
+}
+
+
+
+console.log(findGreater(a,b))
+
+console.log(findGreaters(a,b))
+
+
+
+// */************* */
+
+
+function checkEqual(a, b) {
+  return a === b? "Equal": "Not Equal"
+  }
+  
+ console.log( checkEqual(1, 2))
