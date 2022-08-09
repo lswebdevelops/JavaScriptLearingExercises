7cd66e42098d9deb8e972dc2a39b7c416670a85a
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-08 21:23:19 -0300

    random numbers using Math.floor(Math.random())

--- script.js
+++ script.js
@@ -1,43 +1,20 @@
-// Setup
-const contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
-];
+// function randomFraction() {
 
-function lookUpProfile(name, prop) {
+//   // Only change code below this line
 
-  for (let i = 0; i < contacts.length; i++){
-      if(contacts[i].firstName === name){
-        if(contacts[i].hasOwnProperty(prop)){
-          return contacts[i][prop];
-        }else{
-          return 'No such property';
-        }
-      }
-    }
-  return "No such contact";
+//   return Math.floor(Math.random() * 20);
+
+//   // Only change code above this line
+// }
+
+// console.log(randomFraction()  )
+
+
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+  
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+
+  // Only change code above this line
 }
-
-console.log(lookUpProfile("Sherlock", "lastName"))
+console.log(randomRange(0, 20));
