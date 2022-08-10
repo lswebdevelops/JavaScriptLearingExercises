39266b2126fd4cd7927a84075b4b58fa7508afb6
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-09 22:53:26 -0300

    recursive function if loop start end number

--- script.js
+++ script.js
@@ -1,55 +1,16 @@
-// let array = [13,20,30];
-// let n = array.length;
-
-
-// function sum(arr, n) {
-//   let sum = 0;
-//   for (let i = 0; i < n; i++) {
-//     sum  += arr[i];
-//   }
-//   return sum;
-// }
-
-// console.log(sum(array,n));
-
-
-// // now a recursive function 
-
-
-// function sum2(arr, n) {
-//   if (n <= 0) {
-//     return 0;
-//   } else {
-//     return sum2(arr, n - 1) + arr[n - 1];
-//   }
-// }
-//   console.log(sum2(array,n));
-
-
-function countup(n) {
-  if (n < 1) {
-    return [];
-  } else {
-    const countArray = countup(n - 1);
-    countArray.push(n);
-    return countArray;
-  }
-}
-
-console.log(countup(5));
-
-
 // Only change code below this line
-function countdown(n){
-  if (n < 1) {
-   return [];
+function countUp(startNum,endNum){
+  
+   if (endNum - startNum === 0 ) {
+   return [startNum];
     
-  } else {
-    const countArray = countdown(n - 1);
-    countArray.unshift(n);
-    return countArray;
-  }
-}
-// Only change code above this line
+   } else {
+     const countArray = countUp(startNum, endNum - 1);
+     countArray.push(endNum);
+     return countArray;
+   }
+ 
+ }
+ // Only change code above this line
+console.log(countUp(-10,10))
 
-console.log(countdown(10))
