816cd573732b987fc01f6cfe976f73ae2e465201
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-21 20:33:11 -0300

    creating a div with id, class , attribute, textcontent and title and printing it to the DOM

--- script.js
+++ script.js
@@ -1,55 +1,25 @@
-// transversing the DOM
+// create elements
 
-  let itemList = document.querySelector('#items')
-//   console.log(itemList);
-//   console.log(itemList['parentNode']);
-//   console.log(itemList.parentNode);// or.... 
+// create a div
 
-//itemList.parentNode.parentNode.parentNode.style.backgroundColor= "#333"// now the body has the background.
-
-// itemList.parentNode.parentNode.style.backgroundColor= "#f4f4f4"// body
-
-// console.log(itemList.parentNode.parentNode.parentNode)// html
-
-// childNodes >> should not be used, because when you have breaks between items (like <li></li>'enter' li></li> it
-// will create a textNode in between see below:)
-console.log(itemList.childNodes);
-// ex: 
-// 0: text
-// 1: li.list-group-item
-// 2: text
-// 3: li.list-group-item
-// 4: text
-// instead, use children: 
-console.log(itemList.children)
-
-// 0: li.list-group-item
-// 1: li.list-group-item
-// 2: li.list-group-item
-// 3: li.list-group-item
-// 4: li.list-group-item
-
-console.log(itemList.children[1])
-
-itemList.children[2].style.color = "blue";
-itemList.childNodes[3].style.color = "red"; // should not be used.
-//first Child
-
-// console.log(itemList.firstChild);// do not use either
-console.log(itemList.firstElementChild)// use this instead
-itemList.firstElementChild.textContent = 'firstElementChild';
-itemList.firstElementChild.style.backgroundColor = '#333'
-itemList.firstElementChild.style.color = 'white'
-
-//last element child
-itemList.lastElementChild.style.color = 'firebrick'
+var newDiv = document.createElement('div');
+// console.log(newDiv);
+//add class
+newDiv.className = "hello"
+// console.log(newDiv.className);
+//add id
+newDiv.id = "hello";
+//add attribite
+newDiv.setAttribute('title', "Hello Div");
 
 
-//next Sibling
+let newDivText = document.createTextNode('Hello World!');
 
-itemList.nextElementSibling.style.color = "red"
-// previousElementSibling
-itemList.previousElementSibling.style.border = "blue solid 1px"
+//add text to div
+newDiv.appendChild(newDivText)
+console.log(newDiv);
+//now adding it to the DOM
 
-
-
+let hearderContainer = document.querySelector('header .container');
+let h1 = document.querySelector('header h1');
+hearderContainer.insertBefore(newDiv, h1)
