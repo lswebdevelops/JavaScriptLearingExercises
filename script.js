6e8c380b305c9976e03f5c7b512d60e7f1fcbd85
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-21 20:48:07 -0300

    using a button to change text and style using eventListeners

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
-// create elements
+// // create elements
 
-// create a div
+// // create a div
 
-var newDiv = document.createElement('div');
+// var newDiv = document.createElement('div');
+// // console.log(newDiv);
+// //add class
+// newDiv.className = "hello"
+// // console.log(newDiv.className);
+// //add id
+// newDiv.id = "hello";
+// //add attribite
+// newDiv.setAttribute('title', "Hello Div");
+
+
+// let newDivText = document.createTextNode('Hello World!');
+
+// //add text to div
+// newDiv.appendChild(newDivText)
 // console.log(newDiv);
-//add class
-newDiv.className = "hello"
-// console.log(newDiv.className);
-//add id
-newDiv.id = "hello";
-//add attribite
-newDiv.setAttribute('title', "Hello Div");
+// //now adding it to the DOM
+
+// let hearderContainer = document.querySelector('header .container');
+// let h1 = document.querySelector('header h1');
+// hearderContainer.insertBefore(newDiv, h1)
+// hearderContainer.style.backgroundColor = "#f4f4f4"
+// hearderContainer.style.color = 'blue'
+// hearderContainer.style.border = 'blue solid 1px'
+// hearderContainer.style.textAlign = 'center';
+
+// adding eventListeners
+
+// //old way
+// <button class="btn btn-dark" id="button" 
+// onclick="buttonClick(1)">Click Here</button>
+// function buttonClick(){
+//     console.log('button clicked');
+// }
 
 
-let newDivText = document.createTextNode('Hello World!');
+// /new way/ ideal way
+//using a named function
+let button = document.getElementById('button').addEventListener('click', buttonClick)
+// function buttonClick(){
+//         console.log('button clicked');
+//     }
 
-//add text to div
-newDiv.appendChild(newDivText)
-console.log(newDiv);
-//now adding it to the DOM
+    //now using the DOM
 
-let hearderContainer = document.querySelector('header .container');
-let h1 = document.querySelector('header h1');
-hearderContainer.insertBefore(newDiv, h1)
-hearderContainer.style.backgroundColor = "#f4f4f4"
-hearderContainer.style.color = 'blue'
-hearderContainer.style.border = 'blue solid 1px'
-hearderContainer.style.textAlign = 'center';
+function buttonClick(){
+        // console.log('button clicked');
+    document.getElementById('header-title').textContent = "changed";
+    document.querySelector('#main').style.background= '#827e7e';
+    }
 
 
