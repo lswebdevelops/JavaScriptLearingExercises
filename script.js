caeece6aa4a1a2e0288902c55135dae3e1e17296
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-21 18:25:06 -0300

    changing style properties using js

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
+// // examin the document object//
+ console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.documentURI);
+// console.log(document.title);
+// document.title = "test"
+// console.log(document.body);
+
+// console.log(document.all[13].id);
+// let changeText = document.all[13];
+// console.log(changeText);
+
+// changeText.textContent = "test2"
+// console.log(document.all[13]);
+// console.log(document.images[0]['alt']);
+//selectors. 
 
 
-function validateForm(){
+//getElementById
 
-let x = document.getElementById('numb').value;
+console.log(document.getElementById('test'));
+let divId = document.getElementById('test');
+console.log(divId.textContent)
+divId.innerHTML = '<h2>Hello</h2>'
+divId.style.color = "yellow"
+divId.style.border = "solid yellow 1px"
+divId.style.width = "60px"
+divId.style.height = "5px"
 
-let text;
-if(isNaN(x) || x < 1 || x > 999){
-  text = "Input not valid"
-}else{
-  text= "Input OK"
-}
-document.getElementById('demo').innerText = text;
 
 
-}
+let body = document.getElementById('body');
+console.log(body);
+body.style.backgroundColor= '#333';
+
+
+
+
+
+
+
+
+
 
