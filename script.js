d5286a2a567790c6c8721ced2a8d6b58dec3490c
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-21 11:38:05 -0300

    validing form with numbers using js

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 
 function validateForm(){
 
-let form = document.forms["myForm"]['fname'].value;
-if(form == ""){
-  document.write("Please give name! Press 'F5' and try again.")
-return false;
+let x = document.getElementById('numb').value;
+
+let text;
+if(isNaN(x) || x < 1 || x > 999){
+  text = "Input not valid"
+}else{
+  text= "Input OK"
 }
+document.getElementById('demo').innerText = text;
 
 
 }
