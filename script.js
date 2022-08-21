78554b44a11410b6ec5a69c216c369eb1713f356
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-21 20:37:09 -0300

    creating a div with id, class , attribute, textcontent and title and printing it to the DOM with styles

--- script.js
+++ script.js
@@ -23,3 +23,9 @@ console.log(newDiv);
 let hearderContainer = document.querySelector('header .container');
 let h1 = document.querySelector('header h1');
 hearderContainer.insertBefore(newDiv, h1)
+hearderContainer.style.backgroundColor = "#f4f4f4"
+hearderContainer.style.color = 'blue'
+hearderContainer.style.border = 'blue solid 1px'
+hearderContainer.style.textAlign = 'center';
+
+
