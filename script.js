14b572997cc39248f99578a4ec2b0a34667f7c13
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-21 22:04:50 -0300

    mouse events

--- script.js
+++ script.js
@@ -1,72 +1,32 @@
-// // create elements
-
-// // create a div
-
-// var newDiv = document.createElement('div');
-// // console.log(newDiv);
-// //add class
-// newDiv.className = "hello"
-// // console.log(newDiv.className);
-// //add id
-// newDiv.id = "hello";
-// //add attribite
-// newDiv.setAttribute('title', "Hello Div");
+//mouse events and mouse moves
+let button = document.getElementById('button');
+let box = document.getElementById('box')
+// button.addEventListener('click', runEvent)
+// button.addEventListener('dblclick', runEvent);// doble click
+// button.addEventListener('mousedown', runEvent)
+// button.addEventListener('mouseup', runEvent)
 
 
-// let newDivText = document.createTextNode('Hello World!');
 
-// //add text to div
-// newDiv.appendChild(newDivText)
-// console.log(newDiv);
-// //now adding it to the DOM
-
-// let hearderContainer = document.querySelector('header .container');
-// let h1 = document.querySelector('header h1');
-// hearderContainer.insertBefore(newDiv, h1)
-// hearderContainer.style.backgroundColor = "#f4f4f4"
-// hearderContainer.style.color = 'blue'
-// hearderContainer.style.border = 'blue solid 1px'
-// hearderContainer.style.textAlign = 'center';
-
-// adding eventListeners
-
-// //old way
-// <button class="btn btn-dark" id="button" 
-// onclick="buttonClick(1)">Click Here</button>
-// function buttonClick(){
-//     console.log('button clicked');
-// }
+box.addEventListener('mouseenter', runEvent);
+// box.addEventListener('mouseover', runEvent);
 
 
-// /new way/ ideal way
-//using a named function
 
-// event object 
-let button = document.getElementById('button').addEventListener('click', buttonClick)
-// function buttonClick(){
-//         console.log('button clicked');
-//     }
+function runEvent(e){
+    console.log('Event type: ' + e.type);
+    box.style.background = 'black'
+    box.style.color = 'white'
 
-    //now using the DOM
-
-function buttonClick(e){
-        // console.log('button clicked');
-    
-//     console.log(e);
-//  console.log(e.clientX);   
-
-// console.log(e.target.id);
-// console.log(e.target.classList);
-var x  = e.clientX ;//prints the position of the mouse on the screen when clicked
-var y = e.clientY;
-
-var output = document.getElementById('output');
-output.innerHTML = '<h2>'+ x +' \< x y \> '+ y+'</h2>';
-// console.log(e.type);
-console.log(output);
-console.log(e.offsetX);//prints the position of the mouse on the element when clicked
+}
+box.addEventListener('mouseleave', runEventoff);
+// box.addEventListener('mouseout', runEventoff);
 
 
-console.log(e.altKey);// prints true if alt held
-    
+
+function runEventoff(e){
+    console.log('Event type: ' + e.type);
+    box.style.background = 'yellow'
+    box.style.color = 'red'
+
 }
