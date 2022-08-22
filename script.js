357cffc10ba998a57683a74526c338ac9e2a3a15
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-21 22:33:04 -0300

    keyPress eventes: print the keystrokes to the screen

--- script.js
+++ script.js
@@ -1,43 +1,10 @@
-//mouse events and mouse moves
-let button = document.getElementById('button');
-let box = document.getElementById('box')
-// button.addEventListener('click', runEvent)
-// button.addEventListener('dblclick', runEvent);// doble click
-// button.addEventListener('mousedown', runEvent)
-// button.addEventListener('mouseup', runEvent)
-
-
-
-// box.addEventListener('mouseenter', runEvent);
-// box.addEventListener('mouseover', runEvent);
-box.addEventListener('mousemove', runEvent);
-
-
+let itemInput = document.querySelector('input[type="text"]');
+let form = document.querySelector('form');
+// console.log(itemInput);
+itemInput.addEventListener('keydown', runEvent);
 
 function runEvent(e){
-    console.log('Event type: ' + e.type);
-    let r = e.offsetX 
-    let g = e.offsetY
-    let b = (e.offsetX + e.offsetY) /2;
-    // box.style.background = 'black'
-    // box.style.color = 'white'
-output.innerHTML = '<h3>Mousex: '+e.offsetX+'</h3><h3>MouseY: '+ e.offsetY+'</h3>';
-box.style.backgroundColor= "rgb("+r+","+g+","+b+")";
-//box.style.backgroundColor= "rgb("+e.offsetX +","+e.offsetY+","+b+")"; // could be also like that
+console.log(e.target.value);
+document.getElementById('output').innerHTML = "<h3>" + e.target.value+ "</h3>"
 
-console.log(r);
-console.log(g);
-console.log(b);
 }
-// box.addEventListener('mouseleave', runEventoff);
-// box.addEventListener('mouseout', runEventoff);
-
-
-
-// function runEventoff(e){
-//     console.log('Event type: ' + e.type);
-//     box.style.background = 'yellow'
-//     box.style.color = 'red'
-
-// }
-
