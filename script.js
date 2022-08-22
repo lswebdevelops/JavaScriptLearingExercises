13deba5995c922432cfd5b899a7e47a8a080fa33
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-22 16:07:41 -0300

    input and change events

--- script.js
+++ script.js
@@ -4,16 +4,21 @@ let form = document.querySelector('form');
 // itemInput.addEventListener('keydown', runEvent);
 // itemInput.addEventListener('keyup', runEvent);
 // itemInput.addEventListener('keypress', runEvent);
-itemInput.addEventListener('focus', runEvent);
-itemInput.addEventListener('blur', runEvent);
-itemInput.addEventListener('cut', runEvent);
-itemInput.addEventListener('paste', runEvent);
-itemInput.addEventListener('copy', runEvent);
+// itemInput.addEventListener('focus', runEvent);
+// itemInput.addEventListener('blur', runEvent);
+// itemInput.addEventListener('cut', runEvent);
+// itemInput.addEventListener('paste', runEvent);
+// itemInput.addEventListener('copy', runEvent);
+itemInput.addEventListener('input', runEvent);
+
+let select = document.querySelector('select');
 
 
-
+select.addEventListener('change', runEvent)
 function runEvent(e){
+console.log(e.target.value);
 console.log("event type: " + e.type);
+
 document.getElementById('output').innerHTML = "<h3>" + e.target.value+ "</h3>"
 
 }
