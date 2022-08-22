9bcd1c3a52ede0c0f99ad7a474fee51c643e1f34
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-22 15:59:43 -0300

    focus, blur, cut, past and copy events

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let itemInput = document.querySelector('input[type="text"]');
 let form = document.querySelector('form');
 // console.log(itemInput);
-itemInput.addEventListener('keydown', runEvent);
+// itemInput.addEventListener('keydown', runEvent);
+// itemInput.addEventListener('keyup', runEvent);
+// itemInput.addEventListener('keypress', runEvent);
+itemInput.addEventListener('focus', runEvent);
+itemInput.addEventListener('blur', runEvent);
+itemInput.addEventListener('cut', runEvent);
+itemInput.addEventListener('paste', runEvent);
+itemInput.addEventListener('copy', runEvent);
+
+
 
 function runEvent(e){
-console.log(e.target.value);
+console.log("event type: " + e.type);
 document.getElementById('output').innerHTML = "<h3>" + e.target.value+ "</h3>"
 
 }
+
