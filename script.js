9b73d6e50ab5ff5e7c11fe4b10d405ca4e9db5f3
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-09-02 22:35:47 -0300

    exercise of JavaScript: object has something print sum, otherwise print 0

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
-// Write the code, one line for each action:
+// Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
 
-// Create an empty object user.
-// Add the property name with the value John.
-// Add the property surname with the value Smith.
-// Change the value of the name to Pete.
-// Remove the property name from the object.
+// If salaries is empty, then the result must be 0.
 
-// let obj = {};
-// obj.name = "John";
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
 
-// obj.surname = "Smith";
-// obj.name = "Peter";
+let sum = salaries.John + salaries.Ann + salaries.Pete;
 
-// delete obj.name;
-// console.log(obj);
-
-
-// Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
-
-// Should work like that:
-
-
-
-
-let schedule = {};
-
-
-
-console.log(schedule);
-
-function isEmpty(obj){
-
-for(let key in obj){
-    return false;
-}return true;
-
+function printSalary(obj){
+  for(let key in obj){
+      return sum;
+  }
+       return 0;
 }
 
-console.log("It is empty: " + isEmpty(schedule));// true
-schedule["8:30"] = 'get up';
-console.log("It is empty: " + isEmpty(schedule));// false
 
+
+console.log(printSalary(salaries));
+console.log(salaries);
+
+delete salaries.John;
+delete salaries.Ann;
+delete salaries.Pete;
+console.log("after object empty:");
+console.log(salaries);
+console.log(printSalary(salaries));
