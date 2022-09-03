9499222ccca5f2fc4888bfa4aadb27c84f439202
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-09-03 08:46:05 -0300

    altering objects and printing them to the screen

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-// Multiply numeric property values by 2
-// importance: 3
-// Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
 
-// For instance:
-// before the call
-let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-};
-
-  function multiplyNumeric(menu){
-      for(let key in menu){
-          if(typeof(menu[key]) === 'number'){
-           menu[key]*2;
-                   }
-  }
+const people = {
+    name: {
+        first:'Bob',
+        last:'Smith',
+    },
 }
- console.log(multiplyNumeric(menu));
 
+console.log(people.name.first);
+console.log(people.name.last);
+people.name.first = 'Davi'
+people.name.last = 'Sardanha'
+
+console.log(people.name.first);
+console.log(people.name.last);
+console.clear();
+
+// person
+const person = {
+    name: ['Bob', 'Smith'],
+    age: 32
+  }
+  const input = prompt('Get age?', 23)
+person.age = input;
+
+
+const input2 = prompt('Get name', "Davi");
+person.name[0] = input2;
+
+const input3 = prompt('Get surname','Sardanha');
+person.name[1] = input3;
+
+document.getElementById('p1').innerText = `${person.name[0]} ${person.name[1]} is ${person.age} year(s) old.`;
