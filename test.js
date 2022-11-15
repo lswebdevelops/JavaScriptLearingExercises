6279aca5f111d45bedbbd28243f615ef04bd7017
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-11-15 19:49:33 -0300

    comparing responsabilit principle to non responsabilit principle:

--- test.js
+++ test.js
@@ -1,3 +1,30 @@
+// no responsabilit principle: 
+							 
+	 
+ 
+
+class Car {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    start() {
+        if (...) { // Logic to determine whether or not the car should start
+            this.errorLog(`The car ${this.make} ${this.model} started.`);
+            return true;
+        }
+        this.errorLog(`The car ${this.make} ${this.model} failed to start.`);
+        return false;
+    }
+
+    errorLog(message) {
+        console.log(message);
+    }
+}
+
+//now with responsabilit principle: 
+
 class ErrorLog {
     static log(message) {
         console.log(message);
@@ -18,4 +45,8 @@ class Car {
         ErrorLog.log(`The car ${this.make} ${this.model} failed to start.`);
         return false;
     }
+
+					   
+							 
+	 
 }
